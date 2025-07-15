e55f71b44cf5f33bc02cf0501e24134e4b613dc5
Thebya <www.dibyabantawa666@gmail.com>
2025-07-15 09:38:52 +0545

    unauthorized

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Home from './pages/home';
 import Employee from './pages/employee';
 import Dashboard from './pages/dashboard';
 import ProtectedRoute from './components/home/utils/protectedRoute';
+import Unauthorized from './components/home/utils/unauthorized';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginForm/>}/>
+        <Route path="/unauthorized" element ={<Unauthorized/>}/>
         <Route path="home" element={
           <ProtectedRoute>
             <Home/>
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import stats from '../components/home/Dashboard/statdata'
+import StatCard from '../components/home/Dashboard/statcard';
 
-function dashboard
-() {
+ function dashboard() {
   return (
-    <div>
-      Dashboard
-    </div>
+    <>
+   
+      <h1 className='text -2xl font-bold text-grey-950'>Dashboard Overview</h1>
+      <div className='flex items-center justify-between'>
+        {stats.map((item,index) => {
+          return(
+            <StatCard
+            key={index}
+            title={item.title}
+            count={item.count}
+            icon={<item.icon/>}
+            color={item.color}
+            />
+          );
+        
+      })}
+      </div>
+  </>  
   )
 }
 
-export default dashboard
 
+export default dashboard
